b89dfd2c77efdb2248a4f8ab64852b6df2c49f46
vicjhdezaes <victor.hernandez9@alu.uclm.es>
2023-04-16 18:20:54 +0200

    segundo commit

--- test_app.py
+++ test_app.py
@@ -7,26 +7,36 @@ def client():
     with app.test_client() as client:
         yield client
 
+#def test_sumar(client):
+#    response = client.get('/sumar?a=3&b=4')
+#    data = json.loads(response.data)
+#    assert response.status_code == 200
+#    assert data['resultado'] == 7
+
 def test_sumar(client):
-    response = client.get('/sumar?a=3&b=4')
+    response = client.get('/sumar', query_string=dict(a=3, b=4))
     data = json.loads(response.data)
     assert response.status_code == 200
     assert data['resultado'] == 7
 
+
 def test_restar(client):
-    response = client.get('/restar?a=3&b=4')
+    #response = client.get('/restar?a=3&b=4')
+    response = client.get('/restar', query_string=dict(a=3, b=4))
     data = json.loads(response.data)
     assert response.status_code == 200
     assert data['resultado'] == -1
 
 def test_multiplicar(client):
-    response = client.get('/multiplicar?a=3&b=4')
+    #response = client.get('/multiplicar?a=3&b=4')
+    response = client.get('/multiplicar', query_string=dict(a=3, b=4))
     data = json.loads(response.data)
     assert response.status_code == 200
     assert data['resultado'] == 12
 
 def test_dividir(client):
-    response = client.get('/dividir?a=12&b=4')
+    #response = client.get('/dividir?a=12&b=4')
+    response = client.get('/dividir', query_string=dict(a=3, b=4))
     data = json.loads(response.data)
     assert response.status_code == 200
     assert data['resultado'] == 3
